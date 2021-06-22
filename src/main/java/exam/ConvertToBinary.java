4156591a367c36d149b1cdc945ac0a2469458439
yogeshms <53409270+yogeshms@users.noreply.github.com>
2021-06-22 12:32:37 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,40 @@
 package exam;
+import java.util.Scanner;
 
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+   interview n,m;
+    String x="";
+    Scanner s=new Scanner(System.in);
+    System.out.print("Enter the decimal no");
+    n=s.nextInt();
+  try
+  {
+  if(n>=0)
+{ // No is positive
+}
+}
+Catch (No negative exception)
+{
+  System.out.println("no is negative");
+}
+    While(n>=0)
+      {
+        int a=n%2;
+        x=a+x;
+        n=n/2;
+
+
+
+
+} 
+
+
+
+
+return "";
   }
+System.out.println(x);
 }
