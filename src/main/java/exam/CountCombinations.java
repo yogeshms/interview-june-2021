f4bab45504e54d52817c63bfc41b29a6f9082178
yogeshms <53409270+yogeshms@users.noreply.github.com>
2021-06-22 13:21:27 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -4,7 +4,27 @@ public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    System.out.println("enter the value for T ");
+    Scanner=new Scanner (System.in);
+    int T=sc.nextInt;
+    System.out.println("enter the value for T ");
+    int W=sc.nextInt();
+   
+  for(int j=0;j<=W;j++)
+{
+
+ for(i=0;i<=T;i++)
+    {
+       String n[]=n+"j";
+}
+}
+
+    
+     
+    
+    
+    
+return 0;
   }
 
 }
